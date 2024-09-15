82223fb8a1f507a8b2e93a7fe0d3c54aa2d9978f
Luka-Peralta-Perez <liku2428@gmail.com>
2024-09-14 23:56:57 -0300

    menu y opciones.

--- main.py
+++ main.py
@@ -1,20 +1,55 @@
 from os import system, name
 from tabulate import tabulate
 
+#identifica el sistema operativo
 def limpiarpantalla():
-   os.system("cls" if os.name == "nt" else "clear")
-  
-  
-def motrar_opciones:
-    op =  menu = [
-        ["1", ""],
-        ["2", "R"],
-        ["3", "Ver carrito"],
-        ["4", "Salir"],
+   system("cls" if name == "nt" else "clear")
+
+
+def mostrar_opciones():
+    opciones = [
+        ["1", "Cargar Compra"],
+        ["2", "Ver compras"],
+        ["3", "Ver descuento del Día"],
+        ["4", "Generar ticket de compra"],
+        ["5", "Agregar productos"],
+        ["6", "Salir"]
     ]
-    print(tabulate(menu, headers=["Opción", "Descripción"], tablefmt="grid"))
-    
+    print(tabulate(opciones, headers=["Opción", "Descripción"], tablefmt='fancy_grid', colalign=("center", "left")))
+
+
 def main():
     while True:
-        
-    
+        limpiarpantalla() #Limpia la pantalla antes de mostrar las opciones.
+        mostrar_opciones()
+        usuario = input("Ingrese una opcion: ")
+
+        if usuario == "1":
+            print("Selecciono la opción 1, 'Cargar Compra'.")
+            pass
+        elif usuario == "2":
+            print("Selecciono la opción 2, 'Ver compras'.")
+            pass
+        elif usuario == "3":
+            print("Selecciono la opción 3, 'Ver descuento del Día'.")
+            pass
+        elif usuario == "4":
+            print("Selecciono la opción 4, 'Generar ticket de compra'.")
+            pass
+        elif usuario == "5":
+            print("Selecciono la opción 5, 'Agregar productos'.")
+            pass
+        elif usuario == "6":
+            print("Saliendo...")
+            break
+        else:
+            print("Opcion invalida, intente de nuevo.")
+
+        input("\nPresione Enter para continuar. \n")  # Pausa antes de limpiar de nuevo
+
+
+if __name__ == "__main__":
+    main()
+
+
+
